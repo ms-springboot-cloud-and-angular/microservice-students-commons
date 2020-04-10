0ada7ff2c7a6f947bb37123bedaf70895fd009e9
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-10 17:24:27 +0200

    New SpringBoot project Students Commons

---
+++ src/main/java/com/joseluisestevez/ms/commons/students/MicroserviceStudentsCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.ms.commons.students;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceStudentsCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceStudentsCommonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joseluisestevez/ms/commons/students/MicroserviceStudentsCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.ms.commons.students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceStudentsCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
